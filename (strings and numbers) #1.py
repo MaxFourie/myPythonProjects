53a45f9db2817bdfeae6f89b8069df91c6d78671
MaxFourie <maxhfourie@gmail.com>
2025-09-21 14:49:44 -0700

    Delete (strings and numbers) #1.py

--- (strings and numbers) #1.py
+++
@@ -1,4 +0,0 @@
-print('hello world')
-radius = float(input(print('what is the radius of your circle?: ')))
-print('the circumference of your circle is:', 2 * 3.14 * radius,'cm')
-print('the area of your circle is:', radius**2 * 3.14,'cm')
