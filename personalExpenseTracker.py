3fe6aa2ec37891ddc83a051a9655a9141a4bcb77
MaxFourie <maxhfourie@gmail.com>
2025-09-21 14:50:07 -0700

    Delete personalExpenseTracker.py

--- personalExpenseTracker.py
+++
@@ -1,7 +0,0 @@
-import pickle
-username = input('Please pick a username: ')
-password = input('Please create a password: ')
-with open('userSecurity','wb') as D:
-    pickle.dump(username.lower(), D)
-    pickle.dump(password, D)
-print('Your data has successfully been created')
